fcf77d859851befe7488199f7c6285e710c97ee6
Markus Wolf <mail@markus-wolf.de>
2020-05-04 20:45:13 +0200

    test: setup testing

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -1,22 +1,29 @@
 import * as path from "path";
 import { runTests } from "vscode-test";
 
-async function main() {
+async function runWorkspace(name: string) {
   try {
-    // The folder containing the Extension Manifest package.json
-    // Passed to `--extensionDevelopmentPath`
     const extensionDevelopmentPath = path.resolve(__dirname, "../../");
+    const extensionTestsPath = path.resolve(__dirname, `./${name}/index`);
+    const workspace = path.resolve(
+      __dirname,
+      `../../src/test/${name}/workspace`
+    );
 
-    // The path to test runner
-    // Passed to --extensionTestsPath
-    const extensionTestsPath = path.resolve(__dirname, "./suite/index");
-
-    // Download VS Code, unzip it and run the integration test
-    await runTests({ extensionDevelopmentPath, extensionTestsPath });
+    await runTests({
+      extensionDevelopmentPath,
+      extensionTestsPath,
+      launchArgs: ["--disable-extensions", workspace],
+    });
   } catch (err) {
-    console.error("Failed to run tests");
+    console.error(`Failed to run tests '${name}'`);
     process.exit(1);
   }
+}
+
+async function main() {
+  await runWorkspace("suite");
+  await runWorkspace("simple");
 }
 
 main();
